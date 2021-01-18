1edb3dfcb309b6c9c2207f87e0c12c2d18477d06
Ekaterina Malygina <katty2007v@yandex.ru>
2021-01-18 16:36:32 +0300

    hw nio server lesson-2

--- cloud-server/src/main/java/nio/NioServer.java
+++ cloud-server/src/main/java/nio/NioServer.java
@@ -19,8 +19,8 @@ public class NioServer {
 
     private final ServerSocketChannel serverChannel = ServerSocketChannel.open();
     private final Selector selector = Selector.open();
-    private final ByteBuffer buffer = ByteBuffer.allocate(5);
-    private Path serverPath = Paths.get("serverDir");
+    private final ByteBuffer buffer = ByteBuffer.allocate(1024);
+    private Path serverPath = Paths.get("serverDir").toAbsolutePath();
 
     public NioServer() throws IOException {
         serverChannel.bind(new InetSocketAddress(8189));
@@ -58,19 +58,74 @@ public class NioServer {
             }
             buffer.clear();
         }
-        String command = msg.toString().replaceAll("[\n|\r]", "");
-        if (command.equals("ls")) {
-            String files = Files.list(serverPath)
-                    .map(path -> path.getFileName().toString())
-                    .collect(Collectors.joining(", "));
-            files += "\n";
-            channel.write(ByteBuffer.wrap(files.getBytes(StandardCharsets.UTF_8)));
+        String smsg = msg.toString().replaceAll("[\n|\r]", "").trim();
+        if (!smsg.isEmpty()) {
+            String command = smsg.split(" ")[0];
+            if (command.equals("ls")) {
+                System.out.println(smsg);
+                String files = Files.list(serverPath)
+                        .map(path -> path.getFileName().toString())
+                        .collect(Collectors.joining(", "));
+                //files += "\n";
+                channel.write(ByteBuffer.wrap(files.getBytes(StandardCharsets.UTF_8)));
+            } else if (command.equals("cd")) {
+                System.out.println(smsg);
+                Path pathTemp = getPath(smsg.substring(2).trim());
+                if (pathTemp.toFile().exists()) {
+                    serverPath = pathTemp;
+                } else {
+                    channel.write(ByteBuffer.wrap("Не найден указанный путь".getBytes(StandardCharsets.UTF_8)));
+                }
+            } else if (command.equals("cat")) {
+                System.out.println(smsg);
+                Path pathTemp = getPath(smsg.substring(3).trim());
+                if (pathTemp.toFile().exists()) {
+                    channel.write(ByteBuffer.wrap(Files.readAllBytes(pathTemp)));
+                } else {
+                    channel.write(ByteBuffer.wrap("Не найден указанный файл".getBytes(StandardCharsets.UTF_8)));
+                }
+            } else if (command.equals("touch")) {
+                System.out.println(smsg);
+                Path pathTemp = getPath(smsg.substring(5).trim());
+                if (pathTemp.toFile().exists()) {
+                    channel.write(ByteBuffer.wrap("Указанный файл уже существует".getBytes(StandardCharsets.UTF_8)));
+                } else {
+                    Files.createFile(pathTemp);
+                }
+            } else if (command.equals("mkdir")) {
+                System.out.println(smsg);
+                Path pathTemp = getPath(smsg.substring(5).trim());
+                if (pathTemp.toFile().exists()) {
+                    channel.write(ByteBuffer.wrap("Указанная директория уже существует".getBytes(StandardCharsets.UTF_8)));
+                } else {
+                    Files.createDirectory(pathTemp);
+                }
+            }
         }
+    }
+
+    private Path getPath(String p) {
+        Path pa;
+        if (p.startsWith("\"") && p.endsWith("\"")) {
+            pa = Paths.get(p.substring(1, p.length() - 1));
+        } else {
+            pa = Paths.get(p);
+        }
+        //System.out.println(pa);
+        Path pathTemp;
+        if (pa.isAbsolute()) {
+            pathTemp = pa;
+        } else {
+            pathTemp = Paths.get(serverPath.toString(), pa.toString()).normalize();
+        }
+        //System.out.println(pathTemp);
+        return pathTemp;
     }
 
     private void handleAccept(SelectionKey key) throws IOException {
         SocketChannel channel = ((ServerSocketChannel) key.channel()).accept();
         channel.configureBlocking(false);
         channel.register(selector, SelectionKey.OP_READ);
+        System.out.println("Socket was accepted");
     }
 }
